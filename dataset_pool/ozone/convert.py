4a86334d3599ca505dd8d0d9401481f23ac8ce7d
Alexandre Lacoste <alex.lacoste.shmu@gmail.com>
2013-11-30 21:44:42 -0500

    allo

---
+++ dataset_pool/ozone/convert.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Nov 30, 2013
+
+@author: alexandre
+'''
+
+info = {
+    "url" : "http://archive.ics.uci.edu/ml/datasets/Ozone+Level+Detection",
+    "name": "Ozone Level Detection",
+    "x_type":"reals",
+    "y_type":"classification",
+}
