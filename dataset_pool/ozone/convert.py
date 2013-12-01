12c281a3ea638091d2a853a77dba55b34fa95575
Alexandre Lacoste <alex.lacoste.shmu@gmail.com>
2013-11-30 23:13:17 -0500

    adding util

--- dataset_pool/ozone/convert.py
+++ dataset_pool/ozone/convert.py
@@ -5,6 +5,11 @@ Created on Nov 30, 2013
 @author: alexandre
 '''
 
+import numpy as np
+from os import path
+from graalUtil.num import uHist
+from mlbench.util import load_file, convert_date
+
 info = {
     "url" : "http://archive.ics.uci.edu/ml/datasets/Ozone+Level+Detection",
     "name": "Ozone Level Detection",
@@ -12,10 +17,12 @@ info = {
     "y_type":"classification",
 }
 
+info['preprocessing'] = "using eighthr.data (eight hour data) and ignoring onehr.data"
 
-def convert(path="."):
-    pass
-
-if __name__ == "__main__":
-    convert()
+def convert(in_dir="raw", out_dir="."):
+    x,y = load_file(path.join(in_dir,"eighthr.data"),delimiter=",",
+        converters={0:convert_date})
+    print uHist(x)
+    print uHist(y)
     
+
